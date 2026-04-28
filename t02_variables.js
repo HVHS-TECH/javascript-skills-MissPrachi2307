eb90566e6a1fab29426594b5d12718da0b3d7ea8
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-04-28 23:53:57 +0000

    Apr 28, 2026, 11:53 PM

--- t02_variables.js
+++ t02_variables.js
@@ -1,1 +1,19 @@
 /* Paste your code from task 1 here */
+/****************************
+Name of task: Task 1 
+****************************/
+console.log("Running t01_introduction.js")
+console.log(/*Insert your message here!*/)
+ //Variables 
+
+
+
+/****************************
+Main 
+****************************/
+
+
+
+/****************************
+Functions
+****************************/
