b0b841fb0266500052edf84a0e6125084c530453
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-04-29 00:02:58 +0000

    Apr 29, 2026, 12:02 AM

--- t02_variables.js
+++ t02_variables.js
@@ -5,7 +5,8 @@ Name of task: Task 1
 console.log("Running t02_variables.js")
 console.log(/*Insert your message here!*/)
  //Variables 
-
+var name = "Pratha"
+var 
 
 
 /****************************
