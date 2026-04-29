6cec7cd3b8b867564149c37eb2a948a1708869c5
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-04-29 00:26:56 +0000

    Apr 29, 2026, 12:26 AM

--- t02_variables.js
+++ t02_variables.js
@@ -16,8 +16,7 @@ Main
 ****************************/
 console.log("Name:"+ userName);
 console.log("Age:"+ userAge);
-console.log("Pocket Money: "+pocketMoney)
-console.log(": "+pocketMoney)
+
 
 /****************************
 Functions
