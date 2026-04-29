082c72f5e451801bdeb0bb2f6c53515075b40e95
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-04-29 00:12:05 +0000

    Apr 29, 2026, 12:12 AM

--- t02_variables.js
+++ t02_variables.js
@@ -3,11 +3,12 @@
 Name of task: Task 1 
 ****************************/
 console.log("Running t02_variables.js")
-console.log(/*Insert your message here!*/)
+console.log(/*Hi concole. As of today I am 16 years old. I have 178 dollars*/)
  //Variables 
 var name = "Pratha"
 var age = 16;
-
+var pocketmoney = 200;
+var currentyear = 11; 
 
 
 /****************************
