edfb50201e08a362c150829a147d3924e2332d8c
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-04-30 01:07:23 +0000

    Apr 30, 2026, 1:07 AM

--- t03_maths.js
+++ t03_maths.js
@@ -1,2 +1,24 @@
 /* Paste your code from task 2 here */
 console.log("Running t03_maths.js")
+/****************************
+Name of task: Task 1 
+****************************/
+console.log("Running t02_variables.js")
+
+ //Variables 
+var userName = "Pratha"
+var userAge = 16;
+var pocketMoney = 200;
+var currentYear = 2026; 
+
+
+/****************************
+Main 
+****************************/
+console.log("Name:"+ userName);
+console.log("Age:"+ userAge);
+console.log("Pocket Money:"+ pocketMoney);
+
+
+/****************************
+Functions
