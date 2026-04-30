03900c97a8355f7801c25ebdd612177a69f97f7e
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-04-30 01:15:44 +0000

    Apr 30, 2026, 1:15 AM

--- t01_introduction.js
+++ t01_introduction.js
@@ -2,7 +2,7 @@
 Name of task: Task 1 
 ****************************/
 console.log("Running t01_introduction.js")
-console.log(/*Insert your message here!*/)
+console.log(/Hi people /)
  //Variables 
 
 
