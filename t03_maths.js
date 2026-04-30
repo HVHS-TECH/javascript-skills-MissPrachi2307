5b08072a824ff82204533371530617e3c80e888d
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-04-30 01:08:40 +0000

    Apr 30, 2026, 1:08 AM

--- t03_maths.js
+++ t03_maths.js
@@ -6,19 +6,12 @@ Name of task: Task 1
 console.log("Running t02_variables.js")
 
  //Variables 
-var userName = "Pratha"
-var userAge = 16;
-var pocketMoney = 200;
-var currentYear = 2026; 
+var num
 
 
 /****************************
 Main 
 ****************************/
-console.log("Name:"+ userName);
-console.log("Age:"+ userAge);
-console.log("Pocket Money:"+ pocketMoney);
-
 
 /****************************
 Functions
