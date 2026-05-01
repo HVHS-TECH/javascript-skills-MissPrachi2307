ae5b7b90715e73414a4179600e3accddfb2981ce
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-05-01 02:37:14 +0000

    May 1, 2026, 2:37 AM

--- t03_maths.js
+++ t03_maths.js
@@ -23,21 +23,3 @@ console.log("Running t03_maths.js")
 /****************************
 Name of task: Task 1 
 ****************************/
-console.log("Running t02_variables.js")
-
- //Variables 
-var num1 = 10; 
-var num2 = 5;
-var num3 = 3;
-var num4 = 2;
-var num5 = 1; 
-var answer;
-
-
-/****************************
-Main 
-****************************/
-console.log("add", num1 + num1)
-answer = num1 + num2 / num2 
-console.log( "equation", answer );
-
