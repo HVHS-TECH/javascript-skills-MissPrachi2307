e2c587935f606896f960b6ce9ed504da7a3d9970
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-05-04 21:37:44 +0000

    May 4, 2026, 9:37 PM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -1,1 +1,49 @@
 /* Paste your code from task 3, Maths here */
+console.log("Running t02_variables.js")
+
+ //Variables 
+var userName = "Pratha"
+var userAge = 16;
+var pocketMoney = 200;
+var currentYear = 2026; 
+
+
+/****************************
+Main 
+****************************/
+console.log("Name:"+ userName);
+console.log("Age:"+ userAge);
+console.log("Pocket Money:"+ pocketMoney);
+
+
+/****************************
+Functions
+****************************/
+console.log("Running t03_maths.js")
+
+ //Variables 
+var userName = "Pratha"
+var userAge = 16;
+var pocketMoney = 200;
+var currentYear = 2026; 
+var randomNumber = 10; 
+var halfMoney = 2;
+
+/****************************
+Main 
+****************************/
+console.log("Hi, console");
+console.log("As of " + currentYear + " you are " + userAge + " years old");
+birthYear= currentYear-16
+console.log("You were bron in " + birthYear);
+futureYear= userAge + randomNumber
+console.log("In 10 years you will be " + futureYear);
+console.log("You have " + 200 + " dollars");
+moneySpent= pocketMoney / halfMoney
+totalamount= 100 + 3 
+console.log("You have spent half of your mone, now you have "+ moneySpent ); 
+console.log("Then you get $3, now you have " + totalamount);
+/****************************
+/****************************
+Functions
+****************************/
