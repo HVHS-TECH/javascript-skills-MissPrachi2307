a8f7e1e47a7dcafb5b02fa044b59f98d4128dd92
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-05-04 21:33:12 +0000

    May 4, 2026, 9:33 PM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,7 +1,7 @@
 console.log("Running T04_Debugging")
 // Set up the size of the page.
 var length = 8;
-var width = 10
+var width = 10; 
 // calculate the area
 var area = 80  ; 
 
@@ -20,22 +20,3 @@ if (perimeter > area){
 }
 
 
-console.log("Running T04_Debugging")
-// Set up the size of the page.
-var length = 8;
-var Width = 10
-var area = 80  ; 
-
-// calculate the perimeter
-var perimeter = 2 *"length" + 2 * "width"; 
-
-// Display the results
-console.log("A area of a piece of paper with a length of "+ 8 +", and a width of "+ 10 +" is "+ area);
-console.log("The same piece of paper has a perimeter of "+perimeter);
-
-// Check, does this result look sensible
-if (perimeter > area){
-    console.log("Yay, your area is larger, this is right for this example")
-}else{
-    console.log("Hmm, your perimeter is larger. It shouldn't be for this example")
-}
