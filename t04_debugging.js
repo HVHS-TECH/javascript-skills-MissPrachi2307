f7cbe984bb72775aa86f7b76cb026e74dc6abf72
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-05-04 21:22:50 +0000

    May 4, 2026, 9:22 PM

--- t04_debugging.js
+++ t04_debugging.js
@@ -1,14 +1,12 @@
 console.log("Running T04_Debugging")
-
+// Set up the size of the page.
+var length = 8;
+var width = 10
 // calculate the area
 var area = length + width;
 
 // calculate the perimeter
 var perimeter = 2 *"length" + 2 * "width"; 
-
-// Set up the size of the page.
-var length = 8;
-var width = 10
 
 // Display the results
 console.log("A area of a piece of paper with a length of "+length+", and a width of "+width+" is "+ area);
