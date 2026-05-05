b9d12918c4821eac0a6e1e25d3731be9a20ef5cd
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-05-05 03:09:52 +0000

    May 5, 2026, 3:09 AM

--- t05_JavaScript_and_HTML.js
+++ t05_JavaScript_and_HTML.js
@@ -44,8 +44,8 @@ totalamount= 100 + 3
 console.log("I have spent half of your mone, now I have "+ moneySpent ); 
 console.log("Then I got $3, now I have " + totalamount);
 
-const OUTPUT = document.getElementById("JavascriptOutput ");
-OUTPUT.innerHTML = "<h2>Added by JavaSprict</h2>";
+const OUTPUT = document.getElementById("JavaScriptOutput ");
+OUTPUT.innerHTML= "<h2>Added by JavaSprict</h2>";
 OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
 OUTPUT.innerHTML += "<p>Second Paragraph line.</p>"; 
 OUTPUT.innerHTML += "<p> Third Paragraph line.</p>";
