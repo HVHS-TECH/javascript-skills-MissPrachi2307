b821ca00648ff0edb52d797343dfc6a3770785cc
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-05-05 08:52:53 +0000

    May 5, 2026, 8:52 AM

--- t06_functions.js
+++ t06_functions.js
@@ -41,31 +41,6 @@ Welcome()
 /****************************
 Functions
 ****************************/
+fu
 
 
-
-/****************************
-Main 
-****************************/
-console.log("Hi, console");
-console.log("As of " + currentYear + " I am " + userAge + " years old");
-birthYear= currentYear-16
-console.log("I was bron in " + birthYear);
-futureYear= userAge + randomNumber
-console.log("In 10 years I will be " + futureYear);
-console.log("I have " + 200 + " dollars");
-moneySpent= pocketMoney / halfMoney
-totalamount= 100 + 3 
-console.log("I have spent half of your mone, now I have "+ moneySpent ); 
-console.log("Then I got $3, now I have " + totalamount);
-
-const OUTPUT = document.getElementById("JavaScriptOutput");
-OUTPUT.innerHTML += "<h2>Added by JavaSprict</h2>";
-OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
-OUTPUT.innerHTML += "<p>Second Paragraph line.</p>"; 
-OUTPUT.innerHTML += "<p> Third Paragraph line.</p>";
-
-/****************************
-/****************************
-Functions
-****************************/
