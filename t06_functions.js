36f234eb9a85e7b59cf1a02eefa5514d29b1690f
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-05-05 08:52:07 +0000

    May 5, 2026, 8:52 AM

--- t06_functions.js
+++ t06_functions.js
@@ -6,7 +6,8 @@ var userName = "Pratha"
 var userAge = 16;
 var pocketMoney = 200;
 var currentYear = 2026; 
-
+var randomNumber = 10; 
+var halfMoney = 2;
 
 /****************************
 Main 
@@ -14,6 +15,24 @@ Main
 console.log("Name:"+ userName);
 console.log("Age:"+ userAge);
 console.log("Pocket Money:"+ pocketMoney);
+console.log("Hi, console");
+console.log("As of " + currentYear + " I am " + userAge + " years old");
+birthYear= currentYear-16
+console.log("I was bron in " + birthYear);
+futureYear= userAge + randomNumber
+console.log("In 10 years I will be " + futureYear);
+console.log("I have " + 200 + " dollars");
+moneySpent= pocketMoney / halfMoney
+totalamount= 100 + 3 
+console.log("I have spent half of your mone, now I have "+ moneySpent ); 
+console.log("Then I got $3, now I have " + totalamount);
+
+const OUTPUT = document.getElementById("JavaScriptOutput");
+OUTPUT.innerHTML += "<h2>Added by JavaSprict</h2>";
+OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
+OUTPUT.innerHTML += "<p>Second Paragraph line.</p>"; 
+OUTPUT.innerHTML += "<p> Third Paragraph line.</p>";
+Welcome()
 
     
 }
@@ -24,13 +43,6 @@ Functions
 ****************************/
 
 
- //Variables 
-var userName = "Pratha"
-var userAge = 16;
-var pocketMoney = 200;
-var currentYear = 2026; 
-var randomNumber = 10; 
-var halfMoney = 2;
 
 /****************************
 Main 
