b58653a88625131747be7438957aa79a9135119e
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-05-08 02:57:38 +0000

    May 8, 2026, 2:57 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -33,9 +33,9 @@ OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
 OUTPUT.innerHTML += "<p>Second Paragraph line.</p>"; 
 OUTPUT.innerHTML += "<p> Third Paragraph line.</p>";
 welcome();
-displayProduct("Chocolate", 4)
-displayProduct("Chips", 3)
-displayProduct("Drinks", 2.50)
+displayProduct("Chocolate ", 4)
+displayProduct("Chips ", 3)
+displayProduct("Drinks ", 2.50)
 
 /****************************
 Functions
