b3fb724763b3d3d24d08a648f8bf63a4d39c15d1
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-05-08 02:55:48 +0000

    May 8, 2026, 2:55 AM

--- t07_functions_parameters.js
+++ t07_functions_parameters.js
@@ -47,5 +47,6 @@ OUTPUT .innerHTML += "<p>Welcome to the shop</p>";
 }
 
 function displayProduct(_name, _price){
-    OUTPUT.interHTML += "<p>" + _name + "costs $" + _price + "</p>";
+ console.log("money")  
+    OUTPUT.innerHTML += "<p>" + _name + "costs $" + _price + "</p>";
 }
