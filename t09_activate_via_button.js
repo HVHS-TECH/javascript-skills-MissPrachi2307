94efd7881dfc1433df7a35850264fa3f1b28769a
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-05-13 00:27:21 +0000

    May 13, 2026, 12:27 AM

--- t09_activate_via_button.js
+++ t09_activate_via_button.js
@@ -36,7 +36,11 @@ Main
     OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
     OUTPUT.innerHTML += "<p>Second Paragraph line.</p>"; 
     OUTPUT.innerHTML += "<p> Third Paragraph line.</p>";
-    welcome();
+    
+ welcome();
+displayProduct("Chocolate ", 4)
+displayProduct("Chips ", 3)
+displayProduct("Drinks ", 2.50)
     }
 
 /****************************
