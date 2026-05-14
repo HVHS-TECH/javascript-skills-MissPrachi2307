274a3a8d997eeba72a13f3f092586b4653098520
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-05-14 00:52:16 +0000

    May 14, 2026, 12:52 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -1,1 +1,60 @@
-/* Paste your code from the last task */
+console.log("t09_activeate_via_button.js")
+
+ //Variables 
+var userName = "Pratha"
+var userAge = 16;
+var pocketMoney = 200;
+var currentYear = 2026; 
+var randomNumber = 10; 
+var halfMoney = 2;
+
+/****************************
+Main 
+****************************/
+
+
+    console.log("Name:"+ userName);
+    console.log("Age:"+ userAge);
+    console.log("Pocket Money:"+ pocketMoney);
+    console.log("Hi, console");
+    console.log("As of " + currentYear + " I am " + userAge + " years old");
+    birthYear= currentYear-16
+    console.log("I was bron in " + birthYear);
+    futureYear= userAge + randomNumber
+    console.log("In 10 years I will be " + futureYear);
+    console.log("I have " + 200 + " dollars");
+    moneySpent= pocketMoney / halfMoney
+    totalamount= 100 + 3 
+    console.log("I have spent half of your mone, now I have "+ moneySpent ); 
+    console.log("Then I got $3, now I have " + totalamount);
+
+    const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
+
+    function start(){
+    OUTPUT.innerHTML += "<h2>Added by JavaSprict</h2>";
+    OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
+    OUTPUT.innerHTML += "<p>Second Paragraph line.</p>"; 
+    OUTPUT.innerHTML += "<p> Third Paragraph line.</p>";
+    
+ welcome();
+displayProduct("Chocolate ", 4)
+displayProduct("Chips ", 3)
+displayProduct("Drinks ", 2.50)
+    }
+
+/****************************
+Functions
+****************************/
+function welcome(){
+//Add a line to the html page
+OUTPUT .innerHTML += "<p>Welcome to the shop</p>";
+OUTPUT.innerHTML = "<h2>Added by JavaSprict</h2>";
+OUTPUT.innerHTML += "<p>Hello " + userName + "</p>";
+OUTPUT.innerHTML += "<p>Second Paragraph line.</p>"; 
+OUTPUT.innerHTML += "<p> Third Paragraph line.</p>";
+
+}
+
+function displayProduct (_name, _price){
+    OUTPUT.innerHTML += "<p>" + _name + "costs $" + _price + "</p>"
+}
