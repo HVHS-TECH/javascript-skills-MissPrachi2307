623f9aa3506d7a7bca8805eac7300cfdf8c69f74
MissPrachi2307 <24364pp@hvhs.school.nz>
2026-05-22 03:00:11 +0000

    May 22, 2026, 3:00 AM

--- t10_input_from_HTML.js
+++ t10_input_from_HTML.js
@@ -32,6 +32,7 @@ function start(){
     totalamount= 100 + 3 
     console.log("I have spent half of your mone, now I have "+ moneySpent ); 
     console.log("Then I got $3, now I have " + totalamount);
+     const OUTPUT = document.getElementById("spaceForJavaScriptOutput");
 }
     
  
